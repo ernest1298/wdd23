3f04ebee803c3a3dae47114b813c94eba6593df1
ernest1298 <97247914+ernest1298@users.noreply.github.com>
2022-06-17 21:36:51 -0600

    ujujj'P{0 -'99p7r86cgj,vhn;l

--- Chamber/js/index.js
+++ Chamber/js/index.js
@@ -59,6 +59,26 @@ if(!stored_date) {
 localStorage.setItem('lastvisited', now_date);
 
 //directory page
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
+
 const requestURL = 'https://ernest1298.github.io/wdd23/data.json';
 const cards = document.querySelector('.cards');
 
